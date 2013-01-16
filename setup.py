3fb0dff73448814cb752923b8a57e76cda8ed4c7
Alex <alex@Alex-Asus.(none)>
2013-01-16 11:37:57 +0200

    C-file included, Cython is not required for installation

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-cmdclasses = {}
-try:
-    from Cython.Distutils import build_ext #@UnresolvedImport
-except ImportError:
-    pass
-else:
-    cmdclasses['build_ext'] = build_ext
-
 setup(
     name='phpserialize',
     version='1.0',
@@ -16,8 +8,6 @@ setup(
     author='Maxim Oransky',
     author_email='maxim.oransky@gmail.com',
     description='Php serialization/deserialization implementation on Python',
-    # packages=packages,
-    cmdclass=cmdclasses,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
@@ -25,6 +15,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     ext_modules=[
-        Extension("phpserialize_speedups", ["phpserialize_speedups.pyx"])
+        Extension("phpserialize_speedups", ["phpserialize_speedups.c"])
     ]
 )
